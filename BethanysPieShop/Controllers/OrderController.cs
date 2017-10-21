25e66f121a75dc1a9adad159375d302f1268b0c6
Sonia Kurtz <kurtzsonia@gmail.com>
2017-10-21 13:28:43 +0200

    Added validation

--- BethanysPieShop/Controllers/OrderController.cs
+++ BethanysPieShop/Controllers/OrderController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BethanysPieShop.Controllers
 {
-    public class OrderController
+    public class OrderController : Controller
     {
         private readonly IOrderRepository _orderRepository;
 
@@ -17,6 +17,35 @@ namespace BethanysPieShop.Controllers
 
         public IActionResult Checkout()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            if (_shoppingCart.ShoppingCartItems.Count == 0)
+            {
+                ModelState.AddModelError("", "Your cart is empty, add some pies first");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _orderRepository.CreateOrder(order);
+                _shoppingCart.ClearCart();
+
+                return RedirectToAction("CheckoutComplete");
+            }
+
+            return View(order);
+        }
+
+        public IActionResult CheckoutComplete()
+        {
+            ViewBag.CheckoutCompleteMessage = "Thanks for your order";
+
             return View();
         }
     }
